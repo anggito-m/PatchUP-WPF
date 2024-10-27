4a125bca9f7712500b6f9e7e23fe44145f70ff83
aDJi2003 <adjidharmawan87@gmail.com>
2024-10-27 10:38:20 +0700

    feat: make tutorial page

---
+++ WpfApp1/Tutorial.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using WpfApp1.Model;
+
+namespace WpfApp1
+{
+    public partial class Tutorial : Page
+    {
+        public ObservableCollection<CardModel> Cards { get; set; }
+
+        public Tutorial()
+        {
+            InitializeComponent();
+
+            Cards = new ObservableCollection<CardModel>
+            {
+                new CardModel { Title = "Tutorial", Description = "Simplify your decisions through our Smart Menu Assistant who will help you.", ImageSource = "/icon/book.png" },
+                // Tambahkan kartu lainnya sesuai kebutuhan
+            };
+
+            DataContext = this;
+        }
+    }
+}
