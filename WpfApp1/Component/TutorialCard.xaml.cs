7f3501210dd09762ffaea41b603f092b9fda29da
aDJi2003 <adjidharmawan87@gmail.com>
2024-11-08 19:43:45 +0700

    feat: make tutorial card component

---
+++ WpfApp1/Component/TutorialCard.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1.Component
+{
+    public partial class TutorialCard : UserControl
+    {
+        public TutorialCard()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(TutorialCard), new PropertyMetadata(string.Empty));
+
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(TutorialCard), new PropertyMetadata(string.Empty));
+
+        public ImageSource Icon
+        {
+            get => (ImageSource)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(ImageSource), typeof(TutorialCard), new PropertyMetadata(null));
+    }
+}
--- WpfApp1/Tutorial.xaml.cs
+++ WpfApp1/Tutorial.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,16 +22,44 @@ namespace WpfApp1
     /// </summary>
     public partial class Tutorial : Page
     {
+        public ObservableCollection<TutorialItem> TutorialItems { get; set; }
         public Tutorial()
         {
             InitializeComponent();
             sidebar.NavigateToPage += Sidebar_NavigateToPage;
+            TutorialItems = new ObservableCollection<TutorialItem>
+            {
+                new TutorialItem("Type 1", "Description of type 1", "../icon/book.png"),
+                new TutorialItem("Type 2", "Description of type 2", "../icon/book.png"),
+                new TutorialItem("Type 3", "Description of type 3", "../icon/book.png"),
+                new TutorialItem("Type 4", "Description of type 4", "../icon/book.png"),
+                new TutorialItem("Type 5", "Description of type 5", "../icon/book.png"),
+                new TutorialItem("Type 6", "Description of type 6", "../icon/book.png"),
+                new TutorialItem("Type 7", "Description of type 7", "../icon/book.png"),
+                new TutorialItem("Type 8", "Description of type 8", "../icon/book.png"),
+                new TutorialItem("Type 9", "Description of type 9", "../icon/book.png")
+            };
+
+            DataContext = this;
         }
         private void Sidebar_NavigateToPage(object sender, string pageName)
         {
             Frame frame = new Frame();
             frame.Navigate(sidebar.Navigate(pageName));
             this.Content = frame;
+        }
+        public class TutorialItem
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Icon { get; set; }
+
+            public TutorialItem(string title, string description, string icon)
+            {
+                Title = title;
+                Description = description;
+                Icon = icon;
+            }
         }
     }
 }
