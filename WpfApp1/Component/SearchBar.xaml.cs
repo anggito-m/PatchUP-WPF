75f9ec063ee7455ba95aa71bc812554ed9f0ca91
aDJi2003 <adjidharmawan87@gmail.com>
2024-11-09 11:29:16 +0700

    feat: make searchbar component and apply to some page

---
+++ WpfApp1/Component/SearchBar.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.Component
+{
+    public partial class SearchBar : UserControl
+    {
+        public SearchBar()
+        {
+            InitializeComponent();
+        }
+        private void SearchTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (SearchTextBox.Text == "Search")
+            {
+                SearchTextBox.Text = "";
+                SearchTextBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void SearchTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(SearchTextBox.Text))
+            {
+                SearchTextBox.Text = "Search";
+                SearchTextBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+    }
+}
